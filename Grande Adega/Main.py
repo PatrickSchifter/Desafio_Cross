c711f27b0e322651859e52d09c8fb09fc732a1d5
PatrickSchifter <95550051+PatrickSchifter@users.noreply.github.com>
2022-02-01 08:29:31 -0300

    Delete Grande Adega directory

--- Grande Adega/Main.py
+++
@@ -1,7 +0,0 @@
-import pandas as pd
-
-final_file = 'K:/CWB/Logistica/Rastreamento/Controle_Monitoramento/Aquivos Grande Adega/Prazos.txt'
-df = pd.read_csv(final_file, ',')
-print(df.columns)
-# df = df.query("Lead_time_Jadlog != '-' & ")
-print(df)
